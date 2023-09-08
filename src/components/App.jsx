83750e5123fd15d0817c7f5d9c882c137d21c94f
okuzmychov <125366168+okuzmychov@users.noreply.github.com>
2023-09-08 14:20:23 +0300

    button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,25 @@ import { ErrorMsg, Layout } from './Layout';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Loader } from './Loader/Loader';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+import { serviceGetImages } from 'api';
+
+export class App extends Component {
+  state = {
+    gallery: [],
+    query: {
+      searchString: '',
+      page: 1,
+      perPage: 12,
+      totalHits: 0,
+      timeStamp: null,
+    },
+    loader: false,
+    error: false,
+    showModal: false,
+    bigImgUrl: '',
+  };
+}
 
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>React homework template</div>;
 };
